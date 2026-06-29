a4eb6a4234dae898d723b3f825a5598786174c4a
rkdckdfhr <rkdckdfhr1@gmail.com>
2026-06-29 12:53:33 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 5.32 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// numbers_len은 배열 numbers의 길이입니다.
+int* solution(int numbers[], size_t numbers_len, int num1, int num2) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(sizeof(int) * num2);
+    for(int i = 0; i < numbers_len; i++)
+    {
+        if(num1 + i == num2) 
+        {
+            answer[i] = numbers[num2];
+            break;
+        }
+        else answer[i] = numbers[num1 + i];
+    }
+    return answer;
+}
